11a69637d6d8233a9c876037ad7e2711c7afd1e2
Thorsten Claus <thorstenclaus@web.de>
2017-11-25 08:34:03 +0100

    Facebook login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
   def avatar_url(user)
-    user_id = Digest::MD5::hexdigest(user.email.downcase)
-
-    "https://www.gravatar.com/avatar/#{user_id}.jpg?d=identical&s=150"
+    user_id = Digest::MD5.hexdigest(user.email.downcase)
+    if user.image
+    user.image #  "https://graph.facebook.com/#{user.id}/picture?type=large"
+    else
+      "https://www.gravatar.com/avatar/#{user_id}.jpg?d=identical&s=150"
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
       return user
     else
 
-
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
