a4376f3f6582a54c7c1c6935b8faa9c9cd5b2735
Thorsten Claus <thorstenclaus@web.de>
2018-03-24 15:01:42 +0100

    moved button a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
+
   protected
 
   def configure_permitted_parameters
