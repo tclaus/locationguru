ef9f2f2a405dee3a64fd1da18f5217d309d35115
Thorsten Claus <thorstenclaus@web.de>
2018-03-24 10:52:13 +0100

    set locales default value to en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,28 +8,39 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname phone_number description])
   end
 
   # Redirect after login
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(_resource_or_scope)
     dashboard_path
   end
 
-   private
+  private
 
-   def set_locale
-     logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-     I18n.locale = extract_locale_from_accept_language_header
-     logger.debug "* Locale set to '#{I18n.locale}'"
-   end
-
-   def extract_locale_from_accept_language_header
-     accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
-     if accept_language
-       return accept_language.scan(/^[a-z]{2}/).first
-     end
-
-     DEFAULT_LANGUAGE
+  # sets the localizaton from request Header
+  def set_locale
+    I18n.locale = get_valid_language
+    logger.debug "* Locale set to '#{I18n.locale}'"
   end
+
+  # Returns a valid language ID. Fall back to a default
+  def get_valid_language
+    locale = extract_locale_from_accept_language_header
+    logger.debug "* Extracted Locale ID: #{locale}"
+    if !locale.blank? &&
+      (locale == "de" ||
+        locale == "en")
+        locale
+    else
+      DEFAULT_LANGUAGE
+    end
+  end
+
+  # extracts the accept-language header
+  def extract_locale_from_accept_language_header
+    accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+    logger.debug "* Accept-Language from header: #{accept_language}"
+    return accept_language.scan(/^[a-z]{2}/).first if accept_language
+ end
 end
