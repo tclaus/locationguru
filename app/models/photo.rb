d5a4f4158f902a4d702f8d87bb1535fd3ae2913b
Thorsten Claus <thorstenclaus@web.de>
2018-03-31 10:10:54 +0200

    set max photo size for header

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :location
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { original: "1440x900", regular: "1200x1200>", medium: "480x320>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
