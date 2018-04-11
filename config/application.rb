2b4bfb7e6fab20e6a86aa1a6993a64aa880fe588
Thorsten Claus <thorsten.claus@quinscape.de>
2018-04-11 21:12:18 +0200

    changed rake redis stuff

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module LocationGuru
     config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
+    config.active_job.queue_adapter = :resque
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
