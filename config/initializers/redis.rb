78e318c8cc0a33d18943e0aa7b751ae230781033
Thorsten Claus <thorstenclaus@web.de>
2018-04-12 11:04:17 +0200

    parse redis URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 
-REDIS = Redis.new(url: ENV["REDIS_URL"])
-logger.info "Use redis from #{ENV['REDISTOGO_URL']}"
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = REDIS
