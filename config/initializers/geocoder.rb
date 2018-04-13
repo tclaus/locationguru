e319eeb5994045131483f6caf6bc66e7caf26da0
Thorsten Claus <thorstenclaus@web.de>
2018-04-13 07:51:10 +0200

    add geocoder cache

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,12 @@
 Geocoder.configure(
   units: :km,
   api_key: ENV['maps_key']
+
+)
+
+Geocoder.configure(
+  # caching (see below for details):
+  # caching (see below for details):
+  :cache => :REDIS,
+  :cache_prefix => "GeoCode_"
 )
