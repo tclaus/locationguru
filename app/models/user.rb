0495d79a149782072c3dc6e8aaef31ae15b0873d
Thorsten Claus <thorstenclaus@web.de>
2018-04-26 08:18:36 +0200

    Added -for free- text in new

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
-# Avatar
-  has_attached_file :avatar, {
-    styles: {
-      medium: "300x300>",
-      thumb: "100x100>"
-      },
-      convert_options: {original: "-strip", medium: "-strip", thumb: "-strip"},
-    default_url: "/assets/empty_avatar.png"
-  }
+  # Avatar
+  has_attached_file :avatar,
+                    styles: {
+                      medium: '300x300>',
+                      thumb: '100x100>'
+                    },
+                    convert_options: { original: '-strip', medium: '-strip', thumb: '-strip' },
+                    default_url: '/assets/empty_avatar.png'
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
@@ -22,30 +21,26 @@ class User < ApplicationRecord
 
   has_many :locations
   has_many :reservations
-  has_many :guest_reviews, class_name: "GuestReview", foreign_key: "guest_id"
-  has_many :host_reviews, class_name: "HostReview", foreign_key: "host_id"
+  has_many :guest_reviews, class_name: 'GuestReview', foreign_key: 'guest_id'
+  has_many :host_reviews, class_name: 'HostReview', foreign_key: 'host_id'
 
   # force set admin role
-   after_find do |user|
-     user.role = "admin" if user.email == ENV['admin_role1']
-     user.role = "admin" if user.email == ENV['admin_role2']
-   end
-
- def activeLocations
-   locations.where('active = true')
- end
-
- def inactiveLocations
-   locations.where('active = false or active is NULL')
- end
-
-def isAdmin
-  if self.role == "admin"
-     return true
-  else
-    return false
+  after_find do |user|
+    user.role = 'admin' if user.email == ENV['admin_role1']
+    user.role = 'admin' if user.email == ENV['admin_role2']
   end
-end
+
+  def activeLocations
+    locations.where('active = true')
+  end
+
+  def inactiveLocations
+    locations.where('active = false or active is NULL')
+  end
+
+  def isAdmin
+    role == 'admin'
+  end
 
   def self.from_omniauth(auth)
     user = User.where(email: auth.info.email).first
@@ -68,24 +63,23 @@ end
     end
   end
 
-def generate_pin
-  self.pin = SecureRandom.hex(2)
-  self.phone_verified = false
-  save
-end
+  def generate_pin
+    self.pin = SecureRandom.hex(2)
+    self.phone_verified = false
+    save
+  end
 
-def send_pin(text)
-  # Todo: als Job? Habe ja ein Jobber..
-  @client = Twilio::REST::Client.new
-  @client.messages.create(
-    from: ENV['twilio_from_phone'],
-    to: self.phone_number,
-    body: text + self.pin
-  )
-end
+  def send_pin(text)
+    # TODO: als Job? Habe ja ein Jobber..
+    @client = Twilio::REST::Client.new
+    @client.messages.create(
+      from: ENV['twilio_from_phone'],
+      to: phone_number,
+      body: text + pin
+    )
+  end
 
-def verify_pin(entered_pin)
-  update(phone_verified: true) if self.pin == entered_pin
-end
-
+  def verify_pin(entered_pin)
+    update(phone_verified: true) if pin == entered_pin
+  end
 end
