510967d898168ed00f6319716679f78e52174303
Thorsten Claus <thorstenclaus@web.de>
2018-07-17 09:17:22 +0200

    set language

--- app/jobs/send_activation_reminder_mails_job.rb
+++ app/jobs/send_activation_reminder_mails_job.rb
@@ -57,19 +57,28 @@ private
 
   def send_not_activated_mail_level1(location)
     logger.info " * Send not activated mail1 to Location_id: #{location.id} with name #{location.listing_name}"
-    I18n.locale = location.user.language_id
+    set_language(location)
     SendNotActivatedMailMailer.with(location: location).first_activation_reminder.deliver_now
   end
 
   def send_not_activated_mail_level2(location)
     logger.info " * Send not activated mail2 to Location_id: #{location.id} with name #{location.listing_name}"
-    I18n.locale = location.user.language_id
+    set_language(location)
     SendNotActivatedMailMailer.with(location: location).second_activation_reminder.deliver_now
   end
 
   def send_not_activated_mail_level3(location)
     logger.info " * Send not activated mail3 to Location_id: #{location.id} with name #{location.listing_name}"
-    I18n.locale = location.user.language_id
+    set_language(location)
     SendNotActivatedMailMailer.with(location: location).last_activation_reminder.deliver_now
   end
+
+  def set_language(location)
+    if !location.user.blank?
+      I18n.locale = location.user.language_id
+    else
+      I18n.locale = "de"
+    end
+  end
+
 end
