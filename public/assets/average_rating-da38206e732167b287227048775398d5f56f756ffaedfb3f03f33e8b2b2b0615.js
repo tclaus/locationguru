71ecff90cf5ba2abd4d92999cdeb42775b3ec26b
Thorsten Claus <thorstenclaus@web.de>
2018-09-23 12:01:11 +0200

    Removed assets folder

--- public/assets/average_rating-da38206e732167b287227048775398d5f56f756ffaedfb3f03f33e8b2b2b0615.js
+++
@@ -1,5 +0,0 @@
-
-$(function () {
-  var rating = $('#average_rating').data('value');
-  $('#average_rating').raty({path: '/assets', readOnly: true, score: rating});
-});
--- public/assets/register_user/accept_gtc-5bd4435fdb1bf24837f3a9a4c5ceb852ec5d9e7c7dc9b36044fa7fccd40798c2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#accept_gtc').on('change', function() {
-    console.log('changes checkbox');
-    $('#submit').prop('disabled',
-      !$('#accept_gtc').prop('checked'));
-  });
-});
