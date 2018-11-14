6094a07ff3f2512caec48a13ecec60cb93487030
Thorsten Claus <thorstenclaus@web.de>
2018-11-14 08:17:12 +0100

    Updated gems

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module LocationGuru
     # -- all .rb files in that directory are automatically loaded.
 
     # Supporting gziped responses
-  config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
 
   end
 end
