ad18dd4db9b84a308b028af3387e86b96f7ba9f4
Thorsten Claus <thorstenclaus@web.de>
2019-04-30 08:17:30 +0200

    set message date in message

--- app/modules/counter_helper.rb
+++ app/modules/counter_helper.rb
@@ -2,16 +2,16 @@
 
 module CounterHelper
   # Load total numbers for each location
-  def load_total_numbers(locations)
+  def self.load_total_numbers(locations)
     locations.each do |location|
       location.total_count = load_total_number(location)
     end
   end
 
   # Returns total sum for location ID
-  def load_total_number(location)
+  def self.load_total_number(location)
     # context : location, type: location_id
-    count = Counter.where(context: 'location_visits_for_user', context_type: location.id).sum('count')
-    count
+    Counter.where(context: 'location_visits_for_user', context_type: location.id)
+    .sum('count')
   end
 end
