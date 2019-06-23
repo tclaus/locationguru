1b5ab24786bae619b7e2882c1cf9cae752c1993e
Thorsten Claus <thorstenclaus@web.de>
2019-06-23 20:09:12 +0200

    set language on signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+
   protected
 
   def after_update_path_for(_resource)
@@ -9,5 +10,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+
+  private
+  def sign_up_params
+    params.require(:user).permit(:language_id, :display_name, :email, :password, :password_confirmation)
   end
 end
