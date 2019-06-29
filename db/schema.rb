eadf4601cef88eb570390fd92bb2dd8079bf3f1d
Thorsten Claus <thorstenclaus@web.de>
2019-06-29 21:24:17 +0200

    Update gems / Ruby (#32)
    Add weekly counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_062721) do
+ActiveRecord::Schema.define(version: 2019_06_29_082558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_06_27_062721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "context_type"
+    t.date "date_of_count"
     t.index ["context"], name: "index_counters_on_context"
     t.index ["context_type"], name: "index_counters_on_context_type"
     t.index ["day"], name: "index_counters_on_day"
