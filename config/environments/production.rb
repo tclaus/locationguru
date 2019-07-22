4ed3e0d3480d9ea902a4e6b50874319c8f072122
Thorsten Claus <thorstenclaus@web.de>
2019-07-22 08:11:55 +0200

    Change assets pipeline config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -86,8 +86,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  # config.i18n.fallbacks = true
-  config.i18n.fallbacks = [I18n.default_locale]
+  config.i18n.fallbacks = true
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
