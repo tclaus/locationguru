95af195fad6e34e7096d1fcbf455b3b2e42f513e
Thorsten Claus <thorstenclaus@web.de>
2020-05-25 21:33:29 +0200

    Updating to ruby 2.6.6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.i18n.fallbacks = true
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
