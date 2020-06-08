8c2499a2abcd10ea9372c159c68ce4361faf7b64
Thorsten Claus <thorstenclaus@web.de>
2020-06-08 22:03:44 +0200

    Adding headlines to pre filtered results list (#108)

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -6,36 +6,46 @@ class GuidesController < ApplicationController
 
   def partyvenues
     # Load locations marked as 'party venues'
-    @locations = Location.where(active: true, kind_type: 1)
-    @simple_locations = create_simple_locations(@locations)
-    render 'clubhouse'
+    locations = Location.where(active: true, kind_type: 1)
+    simple_locations = create_simple_locations(locations)
+    headline = '.partyvenues'
+    render 'pre-filtered-list',
+    locals: {locations: locations, simple_locations: simple_locations, headline: headline}
   end
 
   def multi_purpose_rooms
     # Load locations marked as 'pMehrzweckraum'
-    @locations = Location.where(active: true, kind_type: 6)
-    @simple_locations = create_simple_locations(@locations)
-    render 'clubhouse'
+    locations = Location.where(active: true, kind_type: 6)
+    simple_locations = create_simple_locations(locations)
+    headline = '.multi_purpose_rooms'
+    render 'pre-filtered-list',
+    locals: {locations: locations, simple_locations: simple_locations, headline: headline}
   end
 
   def clubhouses
     # Load locations marked as 'KGV / Clubhouse'
-    @locations = Location.where(active: true, kind_type: 12)
-    @simple_locations = create_simple_locations(@locations)
-    render 'clubhouse'
+    locations = Location.where(active: true, kind_type: 12)
+    simple_locations = create_simple_locations(locations)
+    headline = '.clubhouses'
+    render 'pre-filtered-list',
+    locals: {locations: locations, simple_locations: simple_locations, headline: headline}
   end
 
   def pubs
     # Load locations marked as 'bar'
-    @locations = Location.where(active: true, kind_type: 4)
-    @simple_locations = create_simple_locations(@locations)
-    render 'clubhouse'
+    locations = Location.where(active: true, kind_type: 4)
+    simple_locations = create_simple_locations(locations)
+    headline = '.pubs'
+    render 'pre-filtered-list',
+    locals: {locations: locations, simple_locations: simple_locations, headline: headline}
   end
 
   def restaurants
     # Load locations marked as 'restaurant'
-    @locations = Location.where(active: true, kind_type: 3)
-    @simple_locations = create_simple_locations(@locations)
-    render 'clubhouse'
+    locations = Location.where(active: true, kind_type: 3)
+    simple_locations = create_simple_locations(locations)
+    headline = '.restaurant'
+    render 'pre-filtered-list',
+    locals: {locations: locations, simple_locations: simple_locations, headline: headline}
   end
 end
