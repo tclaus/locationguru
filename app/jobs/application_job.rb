054c3b94912223b8644368070b8d38d06a739109
Thorsten Claus <thorstenclaus@web.de>
2020-06-24 21:27:13 +0200

    Activate Photos reminder Mailer

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,7 +2,6 @@
 
 # base class for generating jobs
 class ApplicationJob < ActiveJob::Base
-
   def localize(location)
     I18n.locale = if !location.user.blank?
                     location.user.language_id
