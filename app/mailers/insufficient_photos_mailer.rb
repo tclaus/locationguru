e3a8d284a66499978998a4d7325b0b17b78ab9e0
Thorsten Claus <thorstenclaus@web.de>
2020-07-25 10:55:34 +0200

    Make use of localized mail templates (#133)

--- app/mailers/insufficient_photos_mailer.rb
+++ app/mailers/insufficient_photos_mailer.rb
@@ -3,7 +3,6 @@
 class InsufficientPhotosMailer < ApplicationMailer
   def send_mail
     @location = params[:location]
-    @edit_url = listing_location_url(@location)
     email_with_name = "LocationGuru.net <#{@location.user.email}>"
     mail(to: email_with_name,
          subject: t('.subject', location_name: @location.listing_name))
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost',
+                                               port: 3000
+                                             }
+
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     domain: ENV['MAIL_SEND_FROM_DOMAIN'],
